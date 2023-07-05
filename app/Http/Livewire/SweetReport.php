b9a17acd15b774a382a3b3ff1c637fad642a05c3
Scott Underwood <scott@underwood.co>
2023-07-05 07:38:15 -0500

    Setup report route + TailwindUI component to display comments.

---
+++ app/Http/Livewire/SweetReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SweetReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.sweet-report');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\SweetReport;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/report', SweetReport::class);
 
 Route::middleware([
     'auth:sanctum',

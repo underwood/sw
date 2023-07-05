92dbfcdf895104d1390af25a9b411e1abe086b26
Scott Underwood <scott@underwood.co>
2023-07-05 08:17:28 -0500

    Setup select to choose the type of report, improved table for displaying report. Candy report completed.

--- app/Http/Livewire/SweetReport.php
+++ app/Http/Livewire/SweetReport.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use Livewire\Component;
 
 class SweetReport extends Component
 {
+
+    public $comments;
+    public $reportType;
+
+    public function mount()
+    {
+        // Comments containing the word 'candy'
+
+        $this->reportType = 'Comments about candy';
+    }
+
     public function render()
     {
+
+        $this->comments = [];
+
+        if ($this->reportType == 'Comments about candy') {
+            $this->comments = Comment::whereRaw('LOWER(comments) LIKE ?', ['%candy%'])->get();
+        } elseif ($this->reportType == 'Comments about calling me') {
+        } elseif ($this->reportType == 'Comments about referrals') {
+        } elseif ($this->reportType == 'Comments about signatures') {
+        }
+
         return view('livewire.sweet-report');
     }
 }
--- resources/views/livewire/sweet-report.blade.php
+++ resources/views/livewire/sweet-report.blade.php
@@ -1,41 +1,10 @@
 <div>
     {{-- Be like water. --}}
-  <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
   <div class="relative z-50 xl:hidden" role="dialog" aria-modal="true">
-    <!--
-      Off-canvas menu backdrop, show/hide based on off-canvas menu state.
-
-      Entering: "transition-opacity ease-linear duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "transition-opacity ease-linear duration-300"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
     <div class="fixed inset-0 bg-gray-900/80"></div>
 
     <div class="fixed inset-0 flex">
-      <!--
-        Off-canvas menu, show/hide based on off-canvas menu state.
-
-        Entering: "transition ease-in-out duration-300 transform"
-          From: "-translate-x-full"
-          To: "translate-x-0"
-        Leaving: "transition ease-in-out duration-300 transform"
-          From: "translate-x-0"
-          To: "-translate-x-full"
-      -->
       <div class="relative mr-16 flex w-full max-w-xs flex-1">
-        <!--
-          Close button, show/hide based on off-canvas menu state.
-
-          Entering: "ease-in-out duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-          Leaving: "ease-in-out duration-300"
-            From: "opacity-100"
-            To: "opacity-0"
-        -->
         <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
           <button type="button" class="-m-2.5 p-2.5">
             <span class="sr-only">Close sidebar</span>
@@ -45,7 +14,6 @@
           </button>
         </div>
 
-        <!-- Sidebar component, swap this element with another sidebar if you like -->
         <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ring-1 ring-white/10">
           <div class="flex h-16 shrink-0 items-center">
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
@@ -55,7 +23,6 @@
               <li>
                 <ul role="list" class="-mx-2 space-y-1">
                   <li>
-                    <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
                     <a href="/report" class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                       <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
@@ -74,7 +41,6 @@
 
   <!-- Static sidebar for desktop -->
   <div class="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-72 xl:flex-col">
-    <!-- Sidebar component, swap this element with another sidebar if you like -->
     <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6 ring-1 ring-white/5">
       <div class="flex h-16 shrink-0 items-center">
         <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
@@ -89,14 +55,9 @@
                   <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
                   </svg>
-                  Projects
+                  Report
                 </a>
               </li>
-
-
-
-
-
             </ul>
           </li>
 
@@ -108,58 +69,57 @@
   <div class="xl:pl-72">
     <main>
       <header>
+<div class="w-1/4 p-4">
+  <label for="location" class="block text-sm font-medium leading-6 text-gray-100">Comment Report Type</label>
+  <select wire:model="reportType" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+    <option>Comments about candy</option>
+    <option>Comments about call me</option>
+    <option>Comments about referrals</option>
+    <option>All Comments</option>
+  </select>
+</div>
+
+
       </header>
 
       <!-- Activity list -->
       <div class="border-t border-white/10 pt-11">
-        <h2 class="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">Latest activity</h2>
+        <h2 class="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">{{ $reportType }}</h2>
         <table class="mt-6 w-full whitespace-nowrap text-left">
-          <colgroup>
-            <col class="w-full sm:w-4/12">
-            <col class="lg:w-4/12">
-            <col class="lg:w-2/12">
-            <col class="lg:w-1/12">
-            <col class="lg:w-1/12">
-          </colgroup>
           <thead class="border-b border-white/10 text-sm leading-6 text-white">
             <tr>
-              <th scope="col" class="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">User</th>
-              <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">Commit</th>
-              <th scope="col" class="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">Status</th>
-              <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20">Duration</th>
-              <th scope="col" class="hidden py-2 pl-0 pr-4 text-right font-semibold sm:table-cell sm:pr-6 lg:pr-8">Deployed at</th>
+              <th scope="col" class="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">Order Id</th>
+              <th scope="col" class="py-2 pl-0 pr-8 font-semibold sm:table-cell">Comment</th>
+              <th scope="col" class="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">Expected Ship Date</th>
             </tr>
           </thead>
           <tbody class="divide-y divide-white/5">
+
+            @forelse($comments as $comment)
+
             <tr>
               <td class="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
                 <div class="flex items-center gap-x-4">
-                  <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-8 w-8 rounded-full bg-gray-800">
-                  <div class="truncate text-sm font-medium leading-6 text-white">Michael Foster</div>
+                  <div class="truncate text-sm font-medium leading-6 text-white">{{$comment->orderid}}</div>
+                </div>
+              </td>
+              <td class="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8 whitespace-normal overflow-auto">
+                <div class="flex gap-x-3">
+                  <div class="font-mono text-sm leading-6 text-gray-400">{{ $comment->comments }}</div>
                 </div>
               </td>
               <td class="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
                 <div class="flex gap-x-3">
-                  <div class="font-mono text-sm leading-6 text-gray-400">2d89f0c8</div>
-                  <span class="inline-flex items-center rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20">main</span>
+                  <div class="font-mono text-sm leading-6 text-gray-400">{{ $comment->shipdate_expected }}</div>
                 </div>
               </td>
-              <td class="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
-                <div class="flex items-center justify-end gap-x-2 sm:justify-start">
-                  <time class="text-gray-400 sm:hidden" datetime="2023-01-23T11:00">45 minutes ago</time>
-                  <div class="flex-none rounded-full p-1 text-green-400 bg-green-400/10">
-                    <div class="h-1.5 w-1.5 rounded-full bg-current"></div>
-                  </div>
-                  <div class="hidden text-white sm:block">Completed</div>
-                </div>
-              </td>
-              <td class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">25s</td>
-              <td class="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-gray-400 sm:table-cell sm:pr-6 lg:pr-8">
-                <time datetime="2023-01-23T11:00">45 minutes ago</time>
-              </td>
+
             </tr>
 
-            <!-- More items... -->
+            @empty
+
+            @endforelse
+
           </tbody>
         </table>
       </div>
